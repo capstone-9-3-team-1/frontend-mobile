de0396b6e26929418158abd101c6d02fa61b852e
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-09-07 12:04:41 -0400

    Adds Nativewind/Tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./screens/*.{js,jsx,}",
+    "./pages/*.{js,jsx,}",
+    "./components/*.{js,jsx,}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
