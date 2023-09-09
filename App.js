c9e983865a874821a61b73d23e918750d86bc122
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-09-09 10:58:36 -0400

    Sets up app router and welcome screen

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import Home from "./src/screens/Home";
-import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainContainer from "./src/navigation/MainContainer";
+import Welcome from "./src/screens/Welcome";
+import { SafeAreaView } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          headerStyle: { backgroundColor: "white" },
+        }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="App" component={MainContainer} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
