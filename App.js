bcf3d1392f933d0b5c9b0b35b1d9431eae3865cf
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-09-10 11:50:26 -0400

    Adds token cache for user auth

--- App.js
+++ App.js
@@ -2,13 +2,31 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import Constants from "expo-constants";
 import React from "react";
-
 import MainContainer from "./src/navigation/MainContainer";
+import { getItemAsync, setItemAsync } from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function App() {
   return (
     <ClerkProvider
       publishableKey={Constants.expoConfig.extra.clerkPublishableKey}
+      tokenCache={tokenCache}
     >
       <NavigationContainer>
         <MainContainer />
