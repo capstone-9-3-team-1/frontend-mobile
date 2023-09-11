cdd5373af1454282424a63d76af87adbd2cf8a4e
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-09-11 08:32:37 -0400

    Adds photo display screen and refactors camera component for readability

--- src/navigation/tabs/CameraScreen.js
+++ src/navigation/tabs/CameraScreen.js
@@ -34,35 +34,29 @@ export default function CameraScreen({ navigation }) {
     );
   }
 
-  let takePic = async () => {
+  if (photo) {
+    return <SubmitPhotoScreen photo={photo} setPhoto={setPhoto} />;
+  }
+
+  return (
+    <OpenCamera
+      setPhoto={setPhoto}
+      cameraRef={cameraRef}
+      navigation={navigation}
+    />
+  );
+}
+
+const OpenCamera = ({ navigation, cameraRef, setPhoto }) => {
+  const takePic = async () => {
     let options = {
       quality: 1,
-      base64: true,
       exif: false,
     };
 
     let newPhoto = await cameraRef.current.takePictureAsync(options);
-    console.log(newPhoto);
-    setPhoto(newPhoto);
+    setPhoto(newPhoto.uri);
   };
-
-  if (photo) {
-    return (
-      <SafeAreaView>
-        <Image
-          className="flex-1 self-stretch"
-          source={{ uri: photo.uri + photo.base64 }}
-        />
-        <Pressable
-          className="items-center"
-          title="Discard"
-          onPress={() => setPhoto(undefined)}
-        >
-          <Text>Disgard</Text>
-        </Pressable>
-      </SafeAreaView>
-    );
-  }
 
   return (
     <View>
@@ -86,4 +80,21 @@ export default function CameraScreen({ navigation }) {
       </Camera>
     </View>
   );
-}
+};
+
+const SubmitPhotoScreen = ({ photo, setPhoto }) => {
+  return (
+    <SafeAreaView className="flex items-center space-y-3">
+      <Image className=" h-5/6 w-5/6 mt-5" source={{ uri: photo }} />
+      <Pressable className="items-center bg-green-500 text-white min-w-[150] px-5 py-3 rounded-md">
+        <Text className="text-white">Submit</Text>
+      </Pressable>
+      <Pressable
+        className="items-center border border-green-500 bg-white  px-5 py-3 rounded-md min-w-[150]"
+        onPress={() => setPhoto(undefined)}
+      >
+        <Text className="text-green-500">Try again</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
