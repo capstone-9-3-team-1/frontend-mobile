08db18ad2371692e2c3fbb8f6e01824896176e95
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-09-18 13:01:23 -0400

    Fixes import

--- src/navigation/tabs/Camera/SuccessScreen.jsx
+++ src/navigation/tabs/Camera/SuccessScreen.jsx
@@ -6,18 +6,15 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { useState, useEffect } from "react";
-import useTempHook from "../../../hooks/useTempHook";
-
 export default function SuccessScreen({ navigation }) {
   const [showLoading, setShowLoading] = useState(true);
 
-  // useEffect(() => {
-  //   let timer1 = setTimeout(() => setShowLoading(false), 3000);
-
-  //   return () => {
-  //     clearTimeout(timer1);
-  //   };
-  // }, []);
+  useEffect(() => {
+    let timer1 = setTimeout(() => setShowLoading(false), 3000);
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, []);
 
   return (
     <SafeAreaView>
