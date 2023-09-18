b0251d020057ae6ec30daa3c1d8e765a54d3eab8
riyaalexander <riyaalexander@Riyas-Air.lan>
2023-09-18 13:00:28 -0400

    Camera Screen Receipt

---
+++ src/screens/CameraSuccess.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, Image, SafeAreaView, ScrollView } from "react-native";
+
+const products = [
+    { name: "Product A", points: 50 },
+    { name: "Product B", points: 30 },
+    { name: "Product C", points: 20 },
+];
+
+export default function ReceiptSummaryPage() {
+    return (
+      <SafeAreaView className="flex items-center justify-center h-full">
+        <View className="flex items-center">
+          <Text className="text-3xl font-semibold text-center">Congrats!</Text>
+          <Image
+            source={require("./celebratory-icon.png")} // Replace with your celebratory icon
+            className="mt-4 w-32 h-32"
+          />
+          <Text className="text-2xl font-semibold mt-4">Receipt Summary</Text>
+          <ScrollView className="max-h-40 mt-2">
+            {products.map((product, index) => (
+              <View key={index} className="flex flex-row justify-between py-2 border-b">
+                <Text className="text-lg">{product.name}</Text>
+                <Text className="text-lg font-semibold">{product.points} Points</Text>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    );
+  }
